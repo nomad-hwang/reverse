69329f989ef2415d80e63af8c773e5e1625c12da
David Hwang <nomad.hwang@gmail.com>
2022-11-22 15:16:34 +0900

    fix: letsencrypt_init.sh to create for two domain

--- script/letsencrypt_init.sh
+++ script/letsencrypt_init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-domain="*.device.example.com"
+domain_1="*.dev.example.com"
+domain_2="dev.example.com"
 
 docker run -it --rm --name certbot \
   -v '/etc/letsencrypt:/etc/letsencrypt' \
   -v '/var/lib/letsencrypt:/var/lib/letsencrypt' \
-  certbot/certbot certonly -d domain --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory
+  certbot/certbot certonly -d $domain_1 -d $domain_2 --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory
 
 # Register cronjob to renew certificate every day at 3am
 sudo crontab -l | { cat; echo "0 3 * * * docker run -it --rm --name certbot -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' certbot/certbot renew"; } | sudo crontab -
